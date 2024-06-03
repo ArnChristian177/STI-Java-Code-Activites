b6152a31a319ad564f70095b26a2987203ba5cfd
ArnChristian177 <asahirei321@gmail.com>
2024-06-03 20:52:47 +0800

    Create Introduction.java

---
+++ activities /Introduction.java
@@ -0,0 +1,15 @@
+//Name & Introduction
+//coded by Arn
+//
+public class Introduction {
+    public static void main(String[] args) {
+        int favNumber = 12;
+        String favCartChar = "Doraemon";
+        char mi = 'V';
+        char nickNameArray[] = {'N', 'i', 'c', 'a'};
+        System.out.println(favNumber + " is my favorite number.");
+        System.out.println("I love " + favCartChar + "!");
+        System.out.println("My name is Veronica " + mi + "." + " Velasquez.");
+        System.out.println("You can call me " + nickNameArray[0] + nickNameArray[1] + nickNameArray[2] + nickNameArray[3]);
+    }
+}
